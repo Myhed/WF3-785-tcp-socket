2bc5d1b8319a25552b4b33fa4e118cec0aa4ed9e
Ben-Rhouma Myhed <benrhouma.myhed@gmail.com>
2021-03-12 11:25:59 +0100

    add feature pseudo user

--- socketTcp.js
+++ socketTcp.js
@@ -1,48 +1,60 @@
 const {createServer} = require('net');
 const chalk = require('chalk');
-
-const suppr = Buffer.from([0x08]);
-const enter = Buffer.from([0x0d, 0x0a]);
 const server = createServer();
 const allUsers = [];
+const nameUsers = [];
 
-function connectionUser(users){
+function connectionUser(users, nameUser){
     users.forEach(user => {
-        user.write(chalk.green('\n\rone user is now connected\n\r'));
-        user.write('ecrivez un message: ');
+        user.write(chalk.green(`\n\r${nameUser} is now connected\n\r`));
+        if(nameUsers[allUsers.indexOf(user)].length === 0){
+            user.write("Veuillez saisir un pseudo: ");
+        }else{
+            user.write('ecrivez un message: ');
+        }
     })
 }
 
 function connectedUser(users, currentUser){
     users.forEach(__ => {
         currentUser.write(chalk.green('\n\r name user is connected\n\r'));
-        currentUser.write('ecrivez un message: ');
     })
 }
 
 function sendMessageUser(users, message){
-    console.log('message sended: ', message);
-    console.log(users.length);
     users.forEach(user => {
-        user.write(`\n\rone use send you message: ${message}\n\r`);
+        user.write(`\n\r ${nameUsers[allUsers.indexOf(user)]} : ${message}\n\r`);
         user.write('ecrivez un message: ');
     })
 }
 
 server.on('connection', function(socket) {
     let usersConnected;
+    let pseudo;
     socket.write('====== Bienvenu sur le Tchat TCP ======\n\n\r');
     allUsers.push(socket);
-    socket.write('ecrivez un message: ');
+    nameUsers.push('');
+    socket.write("Veuillez saisir un pseudo: ");
     usersConnected = allUsers.filter(user => user !== socket);
-    connectionUser(usersConnected);
-    connectedUser(usersConnected, socket);
     socket.on('data', data => {
+        data = data.toString();
         usersConnected = allUsers.filter(user => user !== socket);
-        sendMessageUser(usersConnected, data.toString());
-        socket.write('ecrivez un message: ');
+        const regexPseudo = /^([A-Z]+[a-zA-Z0-9]{4,})/g
+        // console.log(nameUsers[allUsers.length-1].trim() === '');
+        if(nameUsers[allUsers.indexOf(socket)].trim() === ''){
+            pseudo = data.match(regexPseudo);
+            if(pseudo !== null){
+                nameUsers[allUsers.indexOf(socket)] = pseudo[0];
+                connectionUser(usersConnected, pseudo[0]);
+                socket.write('ecrivez un message: ');
+            }else{
+                socket.write("Veuillez saisir un pseudo: ");
+            }
+        }else{
+            sendMessageUser(usersConnected, data);
+            socket.write('ecrivez un message: ');
+        }
     });
-    
 })
 
 server.listen(1337, function(){
