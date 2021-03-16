c7d6bb083d86001ff719bc502c491b9c5a925c2d
Ben-Rhouma Myhed <benrhouma.myhed@gmail.com>
2021-03-16 09:45:47 +0100

    refactor router feature

--- http/http.js
+++ http/http.js
@@ -2,20 +2,11 @@ const http = require('http');
 const url = require('url');
 const queryString = require('querystring');
 const fs = require('fs');
-const {GET} = require('./router');
-
-const htmlHomeResponse = ('<h1>Home<h1>')
-
-const homeResponse = {
-    page: 'home'
-}
-const contactResponse = {
-    page: 'contact'
-}
+const router = require('./router');
 
 // const readFileFilm = fs.readFileSync('./films.json').toString()
 
-const isExistingRoute = (url) => GET.routes.filter(route => route.nameRoute === url);
+const isExistingRoute = (methodUsed, url) => router[methodUsed].routes.filter(route => route.nameRoute === url);
 
 // const readFileIfExists = (path) => {
 //     if(fs.access(`./${path}.json`)){
@@ -25,77 +16,16 @@ const isExistingRoute = (url) => GET.routes.filter(route => route.nameRoute === 
 //     }
 // }
 
-function verifyDataTypeJson(data){
-    const jsonParams = data.toString().substr(1, data.toString().length - 2).split(',');
-    const validateParams = jsonParams.map(param => {
-        console.log(param.trim());
-        return param.trim().match(/^\"[\w]+\":\"[\w]+\"/ig)
-    });
-    if(validateParams.indexOf(null) !== -1){
-        return false;
-    }
-    return true;
-}
-
-
 // console.log("FILMS RESPONSE : ", readFileFilm)
 
 const server = http.createServer(function(request, response){
-
-    const parsedQuery = queryString.parse(request.url, null, null)
     response.writeHead(200, {'content-type' : 'application/json'})
-    switch(request.method){
-        case 'GET':
-            // console.log("PARSED QUERY : ", queryString.parse(request.url, null, null))
-            console.log(request.url)
-            const urlExist = isExistingRoute(request.url);
-            console.log(urlExist);
-            if(urlExist.length > 0){
-                urlExist[0][request.url](request, response);
-            }else{
-                response.end(JSON.stringify({code: 404, message: 'NOT FOUND'}));
-            }
-            // if(request.url === '/'){
-            //     response.writeHead(200);
-            //     response.end(JSON.stringify(homeResponse));
-            // }
-            // else if(request.url === '/contact'){
-            //     response.writeHead(200);
-            //     response.end(JSON.stringify(contactResponse));
-            // }
-            // else if(request.url === '/films'){
-            //     response.writeHead(200);
-            //     response.write(readFileFilm);
-            //     response.end()
-            // }
-            // else{
-            //     response.writeHead(200);
-            //     response.end(readFileIfExists(parsedQuery))
-            // }
-
-        break;
-        case 'POST':
-            if(request.url === '/'){
-                request.on('data', data => {
-                    if(request.headers['content-type'] === 'application/json'){
-                        if(verifyDataTypeJson(data)){
-                            response.writeHead(201);
-                            response.end('resource created successfully')
-                        }else{
-                            response.end(JSON.stringify({code: 400, message: 'incorrecte format json'}))
-                        }
-                    }
-                })
-                // response.end(JSON.stringify(homeResponse));
-            }
-        break;
-        case 'PUT':
-            response.end('REQUEST PUT....');
-        break;
-        case 'DELETE':
-            response.end('REQUEST DELETE....');
-        break;
-            // response.end('method not found only GET,DELETE,PUT,POST is a good request')
+    const urlExist = isExistingRoute(request.method,request.url);
+    console.log(urlExist);
+    if(urlExist.length > 0){
+        urlExist[0][request.url](request, response);
+    }else{
+        response.end(JSON.stringify({code: 404, message: 'NOT FOUND'}));
     }
 });
 
--- http/router.js
+++ http/router.js
@@ -1,3 +1,15 @@
+function verifyDataTypeJson(data){
+    const jsonParams = data.toString().substr(1, data.toString().length - 2).split(',');
+    const validateParams = jsonParams.map(param => {
+        console.log(param.trim());
+        return param.trim().match(/^\"[\w]+\":\"[\w]+\"/ig)
+    });
+    if(validateParams.indexOf(null) !== -1){
+        return false;
+    }
+    return true;
+}
+
 const router = {
     'GET': {
         routes: [
